d51ee484ffdd850740f32ff90f1d65d5a0d0b311
manojkrghanaksh <manojkrghanaksh@gmail.com>
2017-07-23 13:51:03 +0530

    Update Test.java

--- strategy/Test.java
+++ strategy/Test.java
@@ -1,1 +1,19 @@
+package com.manoj.dp.strategy;
+
+public class Test {
+
+	public static void main(String[] a){
+		
+		int[] data = {1,3,5,2,54,3,643,46,90};
+		
+		Context context = new Context(new BubbleSort());
+		context.arrange(data);
+		
+		context = new Context(new QuickSort());
+		context.arrange(data);
+		
+		context = new Context(new MergeSort());
+		context.arrange(data);
+	}
+}
 
